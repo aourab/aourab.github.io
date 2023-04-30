4c4b9be324999d218ac77a10976bd437e7faee9c
Ourab Azad <104265952+aourab@users.noreply.github.com>
2023-04-30 11:52:24 +0600

    Update script.js

--- verifiedclintoftheletter/script.js
+++ verifiedclintoftheletter/script.js
@@ -1,6 +1,7 @@
 const letterBody = document.querySelector("#letter-body");
 const signature = document.querySelector("#signature");
 
-letterBody.innerHTML = "I have been thinking about you a lot lately, and I just wanted to take a moment to express my love for you. You make day better just by being in it, and I am so grateful to have you in my life. I love the way you make me feel, and I promise to always be here for you, through thick and thin. I love you more than words can express.";
+letterBody.innerHTML = "I have been thinking about you a lot lately, and I just wanted to take a moment to express my love for you. You make my day better just by being in it, and I am so grateful to have you in my life. I love the way you make me feel, and I promise to always be here for you, through thick and thin. I love you more than words can express.";
 
 signature.innerHTML = "Forever and always, <br> Ourab Azad";
+
