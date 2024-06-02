9343c2c71b41854fd0b491ea85bd9400fc0c70a8
Ourab Azad <104265952+aourab@users.noreply.github.com>
2024-06-02 15:30:12 +0600

    o

--- verifiedclintoftheletter/script.js
+++
@@ -1,9 +0,0 @@
-const letterBody = document.querySelector("#letter-body");
-const signature = document.querySelector("#signature");
-
-letterBody.innerHTML = "সদ্য জন্ম নেওয়া <br> অসহায় এই শিশুকে নিঃশ্বাসের বাতাস দিয়েছিল হেমন্ত। <br> তুমি আমার কাছে হেমন্তের মতোই প্রিয়। ,<br> আমি তোমাকে ভালবাসি তোরসা…..";
-
-
-
-
-
